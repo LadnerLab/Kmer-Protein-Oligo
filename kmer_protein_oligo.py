d4a21a0611ffb806624c42505236d8347bc50805
Bob Costas <finkzane@gmail.com>
2018-04-13 13:03:55 -0700

    Added imports, shebang, and main method

---
+++ kmer_protein_oligo.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+import optparse
+import sys
+import protein_oligo_library as oligo
+
+
+def main():
+    usage = "usage: %prog [options]"
+    option_parser = optparse.OptionParser( usage )
+    
+    add_program_options( option_parser )
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
