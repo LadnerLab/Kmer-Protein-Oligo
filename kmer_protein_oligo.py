e8607d97ec7cc1c27a7204d0ea886179a440ad01
Bob Costas <finkzane@gmail.com>
2018-04-16 12:07:45 -0700

    Added XmerWindow and YmerWindow size options

--- kmer_protein_oligo.py
+++ kmer_protein_oligo.py
@@ -17,7 +17,7 @@ def main():
     seq_dict = {}
     for index in range( len( sequences ) ):
 
-        # We know the recursion is finite
+        # We know the recursion is finite becuase the sequence is finite
         sys.setrecursionlimit( len( sequences[ index ] ) + 50 )
         name, sequence = oligo.subset_lists( names[ index ], sequences[ index ], options )
 
@@ -34,16 +34,22 @@ def add_program_options( option_parser ):
    option_parser.add_option( '-q', '--query', help = "Fasta query file of sequence to be used by program. [None, Required]"
    )
 
-   option_parser.add_option( '-w', '--windowSize', type = 'int', \
+   option_parser.add_option( '-x', '--XmerWindowSize', type = 'int', \
                              default = 100, \
-                             help = "Amount of characters from each alignment sequence to look at. [100]"
+                             help = "Amount of characters from each Xmer alignment sequence to look at. [100]"
    )
+
+   option_parser.add_option( '-y', '--YmerWindowSize', type = 'int', \
+                             default = 100, \
+                             help = "Amount of characters from each Ymer alignment sequence to look at. [100]"
+   )
+
    option_parser.add_option( '-o', '--outPut', default = "oligo_out.txt", help = "Name of file program output will be written to. [oligo_out.txt]"
    )
 
-   option_parser.add_option( '-r', '--redundancy', type = 'int', default = 8, help = "A number specifying the redundancy to be used to each kmer [8]" )
+   option_parser.add_option( '-r', '--redundancy', type = 'int', default = 8, help = "A number specifying the redundancy to be used to each kmer [3]" )
 
-   option_parser.add_option( '-s', '--stepSize', type = 'int', default = 1, help = (
+   option_parser.add_option( '--StepSize', type = 'int', default = 1, help = (
       "Step size to move over after each subset of windowSize characters has been read"
       )
       )
