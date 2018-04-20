df3bea70f47b515b5766ca86e412c2737742106e
jtladner <jtladner@gmail.com>
2018-04-19 19:15:16 -0700

    Added iterations
    Added option for running multiple iterations and choosing the one with the least final oligos

--- kmer_protein_oligo.py
+++ kmer_protein_oligo.py
@@ -15,102 +15,111 @@ def main():
 
     names, sequences = oligo.read_fasta_lists( options.query )
 
-    xmer_seq_dict = {}
+    min_ymers = 999999999999999999999999999999999
 
-    # create list of Xmer sequences
-    for index in range( len( sequences ) ):
+    for i in range(options.iterations): 
 
-        name, sequence = oligo.subset_lists_iter( names[ index ], sequences[ index ], options.XmerWindowSize, options.stepSize )
+        xmer_seq_dict = {}
 
-        for index in range( len( sequence ) ):
-            if oligo.is_valid_sequence( sequence[ index ], options.minLength, options.percentValid ):
-                value = [ options.redundancy, name[ index ] ]
-                xmer_seq_dict[ sequence[ index ] ] = value
+        # create list of Xmer sequences
+        for index in range( len( sequences ) ):
 
-    # create dict of Ymer sequences
-    ymer_seq_dict = {} 
+            name, sequence = oligo.subset_lists_iter( names[ index ], sequences[ index ], options.XmerWindowSize, options.stepSize )
 
-    # Break each ymer up into subsets of xmer size
-    for index in range( len( sequences ) ):
+            for index in range( len( sequence ) ):
+                if oligo.is_valid_sequence( sequence[ index ], options.minLength, options.percentValid ):
+                    value = [ options.redundancy, name[ index ] ]
+                    xmer_seq_dict[ sequence[ index ] ] = value
 
-        name, sequence = oligo.subset_lists_iter( names[ index ], sequences[ index ], options.YmerWindowSize, options.stepSize )
+        # create dict of Ymer sequences
+        ymer_seq_dict = {} 
+
+        # Break each ymer up into subsets of xmer size
+        for index in range( len( sequences ) ):
+
+            name, sequence = oligo.subset_lists_iter( names[ index ], sequences[ index ], options.YmerWindowSize, options.stepSize )
 
 
-        for index in range( len( sequence ) ):
+            for index in range( len( sequence ) ):
 
-            if oligo.is_valid_sequence( sequence[ index ], options.minLength, options.percentValid ):
-                ymer_seq_dict[ sequence[ index ] ] = name[ index ]
+                if oligo.is_valid_sequence( sequence[ index ], options.minLength, options.percentValid ):
+                    ymer_seq_dict[ sequence[ index ] ] = name[ index ]
 
-    total_ymers = len(ymer_seq_dict)
+        total_ymers = len(ymer_seq_dict)
 
-    array_design = {}
-    array_xmers = {}
-    to_add = []
-    ymer_xmers = []
-    iter_count = 0
+        array_design = {}
+        array_xmers = {}
+        to_add = []
+        ymer_xmers = []
+        iter_count = 0
 
-    while True:
-        #reset max score at the beginning of each iteration
-        max_score = 0
-        for current_ymer in ymer_seq_dict.keys(): 
-            # calculate the score of this ymer
-            score, subset_ymer = calculate_score( current_ymer, xmer_seq_dict, options.XmerWindowSize, 1 )
+        while True:
+            #reset max score at the beginning of each iteration
+            max_score = 0
+            for current_ymer in ymer_seq_dict.keys(): 
+                # calculate the score of this ymer
+                score, subset_ymer = calculate_score( current_ymer, xmer_seq_dict, options.XmerWindowSize, 1 )
             
-            if score > max_score:
-                to_add = list()
-                max_score = score
-                to_add.append( current_ymer )
-                ymer_xmers = [subset_ymer]
-            elif score == max_score:
-                to_add.append( current_ymer )
-                ymer_xmers.append(subset_ymer)
+                if score > max_score:
+                    to_add = list()
+                    max_score = score
+                    to_add.append( current_ymer )
+                    ymer_xmers = [subset_ymer]
+                elif score == max_score:
+                    to_add.append( current_ymer )
+                    ymer_xmers.append(subset_ymer)
 
-        random_index = random.choice( range( len( to_add ) ) )
-        oligo_to_remove = to_add[ random_index ]
-        chosen_xmers = ymer_xmers[ random_index ]
-#        array_xmers.update(chosen_xmers)
-        for each in chosen_xmers:
-            array_xmers[each] = array_xmers.get(each, 0)+1
+            random_index = random.choice( range( len( to_add ) ) )
+            oligo_to_remove = to_add[ random_index ]
+            chosen_xmers = ymer_xmers[ random_index ]
+    #        array_xmers.update(chosen_xmers)
+            for each in chosen_xmers:
+                array_xmers[each] = array_xmers.get(each, 0)+1
 
-        # subtract from the score of each xmer within the chosen ymer
-        for item in chosen_xmers:
-            if item in xmer_seq_dict:
-            # We dont' want negative scores
-                if xmer_seq_dict[ item ][ 0 ] > 0:
-                    xmer_seq_dict[ item ][ 0 ] -= 1
-            else: print ( "%s - not found in xmer dict!!!" % (item) )
+            # subtract from the score of each xmer within the chosen ymer
+            for item in chosen_xmers:
+                if item in xmer_seq_dict:
+                # We dont' want negative scores
+                    if xmer_seq_dict[ item ][ 0 ] > 0:
+                        xmer_seq_dict[ item ][ 0 ] -= 1
+                else: print ( "%s - not found in xmer dict!!!" % (item) )
 
-        iter_count += 1
+            iter_count += 1
 
-        if len( ymer_seq_dict ) == 0 or max_score <= 0:
-            print ( "Final design includes %d %d-mers (%.1f%% of total) " % (len(array_design), options.YmerWindowSize, (len(array_design)/float(total_ymers))*100) )
-#            average_redundancy = sum( xmer_seq_dict[ item ][ 0 ] for item in xmer_seq_dict ) / len( xmer_seq_dict )
-            print( "%d unique %d-mers in final %d-mers (%.2f%% of total)" % (len(array_xmers), options.XmerWindowSize, options.YmerWindowSize, (float(len(array_xmers))/len(xmer_seq_dict))*100))
-            print( "Average redundancy of %d-mers in %d-mers: %.2f" % (options.XmerWindowSize, options.YmerWindowSize, sum(array_xmers.values())/float(len(array_xmers))))
-
-            break
+            if len( ymer_seq_dict ) == 0 or max_score <= 0:
+                print ( "Final design includes %d %d-mers (%.1f%% of total) " % (len(array_design), options.YmerWindowSize, (len(array_design)/float(total_ymers))*100) )
+    #            average_redundancy = sum( xmer_seq_dict[ item ][ 0 ] for item in xmer_seq_dict ) / len( xmer_seq_dict )
+                print( "%d unique %d-mers in final %d-mers (%.2f%% of total)" % (len(array_xmers), options.XmerWindowSize, options.YmerWindowSize, (float(len(array_xmers))/len(xmer_seq_dict))*100))
+                print( "Average redundancy of %d-mers in %d-mers: %.2f" % (options.XmerWindowSize, options.YmerWindowSize, sum(array_xmers.values())/float(len(array_xmers))))
+                if len(array_design)<min_ymers:
+                    min_ymers = len(array_design)
+                    best_xmer_seq_dict = xmer_seq_dict
+                    del(xmer_seq_dict)
+                    best_array_design = array_design
+                    del(array_design)
+                break
         
 
-        try:
-            array_design[ oligo_to_remove ] = ymer_seq_dict[ oligo_to_remove ]
-            del ymer_seq_dict[ oligo_to_remove ]
-        except KeyError:
-            continue
+            try:
+                array_design[ oligo_to_remove ] = ymer_seq_dict[ oligo_to_remove ]
+                del ymer_seq_dict[ oligo_to_remove ]
+            except KeyError:
+                continue
 
-        if not iter_count % 100:
-            print( "Current Iteration: " + str( iter_count ) ) 
-#            print( "Number of output ymers: " + str( len( array_design ) ) )
-            print( "Current xmer dictionary score: " + str( sum( item[ 0 ] for item in xmer_seq_dict.values() ) ) )
+            if not iter_count % 250:
+                print( "Current Iteration: " + str( iter_count ) ) 
+    #            print( "Number of output ymers: " + str( len( array_design ) ) )
+                print( "Current xmer dictionary score: " + str( sum( item[ 0 ] for item in xmer_seq_dict.values() ) ) )
 
 
 
-    write_outputs( xmer_seq_dict, options.outPut )
+    write_outputs( best_xmer_seq_dict, options.outPut )
 
     names = []
     sequences = []
 
     # Write resulting oligos to file
-    for sequence, name in array_design.items():
+    for sequence, name in best_array_design.items():
         names.append( name )
         sequences.append( sequence )
 
@@ -162,6 +171,8 @@ def add_program_options( option_parser ):
    )
 
    option_parser.add_option( '-r', '--redundancy', type = 'int', default = 3, help = "A number specifying the redundancy to be used to each kmer [3]" )
+
+   option_parser.add_option( '-i', '--iterations', type = 'int', default = 1, help = "Number of independent iterations to run. The result with the fewest oligos will be output [1]" )
 
    option_parser.add_option( '--stepSize', type = 'int', default = 1, help = (
       "Step size to move over after each subset of windowSize characters has been read [1]"
